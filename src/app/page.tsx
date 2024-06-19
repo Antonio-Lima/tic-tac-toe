26ed78bb14f4feca3da3793fb4e29414e12752f4
Antonio-Lima <gutto1998@gmail.com>
2024-06-19 15:59:19 -0300

    Ajuste.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import DifficultySelector from "@/components/DifficultySelector";
 import Game from "@/components/Game";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-around p-24">
       <h1 className="text-5xl font-bold text-blue-600">Jogo da Velha</h1>
-      <DifficultySelector />
-      <Game />
+      <Suspense>
+        <DifficultySelector />
+      </Suspense>
+      <Suspense>
+        <Game />
+      </Suspense>
     </main>
   );
 }
